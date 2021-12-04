c8d86578fcdf3a760f7333629a7c4704b1775450
Sharone Nixon <shanix@live.de>
2021-12-04 11:06:48 +0100

    added 404 response

--- server/index.js
+++ server/index.js
@@ -22,12 +22,9 @@ app.get('/', function (req, res) {
 
 
 
-
-
-
-
-
-
+app.all("*", (req, res) => {
+    res.send("Sorry, we couldn't find this website")
+})
 
 app.listen(4000, (req, res) => {
     console.log("listening on PORT 4000")
